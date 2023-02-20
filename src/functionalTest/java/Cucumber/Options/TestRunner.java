9f274da1a18b6741ee24ba26a44bb22e44712a8f
marianadpereira <mariana.pereira@hmcts.net>
2023-02-20 13:59:23 +0000

    small refactorings

--- src/functionalTest/java/Cucumber/Options/TestRunner.java
+++ src/functionalTest/java/Cucumber/Options/TestRunner.java
@@ -7,10 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/functionalTest/java/uk/gov/hmcts/reform/hmc/jp/functional/features",
-//    plugin = {"pretty",
-//        "html:target/cucumberReports/cucumber-report.html",
-//        "json:target/cucumberReports/cucumber-report.json"
-//    },
+    plugin = {"pretty",
+        "html:target/cucumberReports/cucumber-report.html",
+        "json:target/cucumberReports/cucumber-report.json"
+    },
     glue = {"uk.gov.hmcts.reform.hmc.jp.functional.stepDefinitions"})
 public class TestRunner {
 }
