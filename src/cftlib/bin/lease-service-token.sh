b2994d0919467671894eef8b24da9654cb76545d
rebeccabaker <rebecca.baker@hmcts.net>
2023-02-24 14:07:28 +0000

    IJPS-16 introduce cftlib for end to end testing along with scripts to obtain bearer tokens

---
+++ src/cftlib/bin/lease-service-token.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+microservice=${1:-ccd_gw}
+
+curl --insecure --fail --show-error --silent -X POST \
+  ${SERVICE_AUTH_PROVIDER_API_BASE_URL:-http://localhost:8489}/testing-support/lease \
+  -H "Content-Type: application/json" \
+  -d '{
+    "microservice": "'${microservice}'"
+  }' \
+  -w "\n"
