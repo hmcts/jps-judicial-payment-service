f7058d035a28fe0e02468e7570d81fd912d91f9c
tomforni <Thomas.Forni@hmcts.net>
2023-03-16 12:28:41 +0000

    defined primary key

--- src/main/resources/db/migration/V20230314_21__IJPS-21-JudicialPaymentService .sql
+++ src/main/resources/db/migration/V20230314_21__IJPS-21-JudicialPaymentService .sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.judicial-payment-service (
+CREATE TABLE public.judicial_payment_service (
                              fee_record_id bigint not null,
                              hmcts_service_id varchar(60) not null,
                              fee_id varchar(60) not null,
@@ -9,3 +9,6 @@ CREATE TABLE public.judicial-payment-service (
                              effective_from timestamp without time zone not null,
                              pensionable_code integer,
 );
+
+ALTER TABLE ONLY public.judicial_payment_service
+    ADD CONSTRAINT payment_service_pkey PRIMARY KEY (fee_record_id);
