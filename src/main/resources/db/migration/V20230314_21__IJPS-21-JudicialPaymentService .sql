04f5aa082d22b9580eeb807680f596d44724ac89
tomforni <Thomas.Forni@hmcts.net>
2023-03-24 00:12:07 +0000

    Changed name of table

--- src/main/resources/db/migration/V20230314_21__IJPS-21-JudicialPaymentService .sql
+++ src/main/resources/db/migration/V20230314_21__IJPS-21-JudicialPaymentService .sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.judicial_payment_service (
+CREATE TABLE public.judicial_fee (
                              fee_record_id bigint not null,
                              hmcts_service_id varchar(60) not null,
                              fee_id varchar(60) not null,
@@ -10,20 +10,20 @@ CREATE TABLE public.judicial_payment_service (
                              pensionable_code integer,
 );
 
-ALTER TABLE ONLY public.judicial_payment_service
+ALTER TABLE ONLY public.judicial_fee
     ADD CONSTRAINT payment_service_pkey PRIMARY KEY (fee_record_id);
 
-ALTER TABLE ONLY public.judicial_payment_service
+ALTER TABLE ONLY public.judicial_fee
 ADD CONSTRAINT uc_judicial_payment_service_fee_id UNIQUE (fee_id);
 
-CREATE SEQUENCE public.judicial_payment_service_id_seq
+CREATE SEQUENCE public.judicial_fee_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
-ALTER TABLE ONLY public.judicial_payment_service
+ALTER TABLE ONLY public.judicial_fee
 ALTER COLUMN request_id SET DEFAULT nextval('public.judicial_payment_service_id_seq');
 
-ALTER SEQUENCE public.judicial_payment_service_id_seq OWNED BY public.judicial_payment_service.fee_id;
+ALTER SEQUENCE public.judicial_fee_id_seq OWNED BY public.judicial_fee.fee_id;
