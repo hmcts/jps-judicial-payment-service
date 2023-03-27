6541ce2c8dd7409667e5a6bab718a8b33221d8c5
RebeccaBaker <38425793+RebeccaBaker@users.noreply.github.com>
2023-03-27 08:51:05 +0100

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -8,7 +8,7 @@ terraform {
      }
     azurerm = {
       source  = "hashicorp/azurerm"
-       version = "3.33.0"      # AzureRM provider version
+      version = "~> 3.49.0"       # AzureRM provider version
     }
   }
 }
