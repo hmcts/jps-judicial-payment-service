b841ff0966687e7b27991368f9578d52a5a0f8f0
SomeshRA <somesh.acharya1@hmcts.net>
2023-04-12 15:17:02 +0100

    IJPS-39: Fixed checkstyle

--- src/main/java/uk/gov/hmcts/reform/jps/expection/PendingMigrationScriptException.java
+++ src/main/java/uk/gov/hmcts/reform/jps/expection/PendingMigrationScriptException.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.jps.expection;
 
 public class PendingMigrationScriptException extends RuntimeException {
     private static final long serialVersionUID = -7544166728568238022L;
+
     public PendingMigrationScriptException(String script) {
         super("Found migration not yet applied " + script);
     }
