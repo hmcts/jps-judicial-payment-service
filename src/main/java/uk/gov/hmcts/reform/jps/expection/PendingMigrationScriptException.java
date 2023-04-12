3d73173fa569a83d42d968585ed0224af44e0560
SomeshRA <somesh.acharya1@hmcts.net>
2023-04-12 15:13:32 +0100

    IJPS-39: Fixed integration PendingMigrationScriptException.java  pmd issue

--- src/main/java/uk/gov/hmcts/reform/jps/expection/PendingMigrationScriptException.java
+++ src/main/java/uk/gov/hmcts/reform/jps/expection/PendingMigrationScriptException.java
@@ -1,7 +1,8 @@
 package uk.gov.hmcts.reform.jps.expection;
 
-public class PendingMigrationScriptException extends RuntimeException {
 
+public class PendingMigrationScriptException extends RuntimeException {
+    private static final long serialVersionUID = -7544166728568238022L;
     public PendingMigrationScriptException(String script) {
         super("Found migration not yet applied " + script);
     }
