76acf87a39d7bc52a0b000dfc093946a5e677fc1
SomeshRA <somesh.acharya1@hmcts.net>
2023-04-12 16:03:17 +0100

    IJPS-39: Added build ignore and test for FlywayNoOpStrategyTest

--- src/main/java/uk/gov/hmcts/reform/jps/expection/PendingMigrationScriptException.java
+++ src/main/java/uk/gov/hmcts/reform/jps/expection/PendingMigrationScriptException.java
@@ -1,8 +1,7 @@
 package uk.gov.hmcts.reform.jps.expection;
 
-
+@SuppressWarnings("serial")
 public class PendingMigrationScriptException extends RuntimeException {
-    private static final long serialVersionUID = -7544166728568238022L;
 
     public PendingMigrationScriptException(String script) {
         super("Found migration not yet applied " + script);
