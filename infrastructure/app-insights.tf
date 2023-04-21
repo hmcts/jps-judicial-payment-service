7afb03a1befdda0566c61ed839a778543443e8aa
Viktoriya Bogdanova <viktoriyabogdanova@jpvft6l5c4.home>
2023-04-21 15:54:52 +0100

    typo

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -20,5 +20,5 @@ data "azurerm_key_vault" "key_vault" {
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = local.app_insights_config
-  key_vault_id = data.key-vault.key_vault_id
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
