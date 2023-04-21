62ce013adab9cb315188a1eba3d63822c93120ed
SomeshRA <somesh.acharya1@hmcts.net>
2023-04-21 09:57:24 +0100

    IJPS-48: Appsinsight updates

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -1,5 +1,19 @@
+data "azurerm_application_insights" "ai" {
+  name                = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-shared-infrastructure-${var.env}"
+}
+
+locals {
+  app_insights_config = jsonencode(
+    merge(
+      jsondecode(
+      file("${path.module}/../lib/applicationinsights.json")), { connectionString = data.azurerm_application_insights.ai.connection_string }
+    )
+  )
+}
+
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = locals.app_insights_config
   key_vault_id = module.key-vault.key_vault_id
 }
