3676df7aa2e07b2c40b0997ef6c9f3921a762c50
SomeshRA <somesh.acharya1@hmcts.net>
2023-05-03 10:05:54 +0100

    IJPS-39: Fixed checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/jps/repository/SittingRecordRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/jps/repository/SittingRecordRepositoryTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.jps.repository;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
