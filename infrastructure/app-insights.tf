96387fc44da1513580e0a6ad279944b59369a306
SomeshRA <somesh.acharya1@hmcts.net>
2023-05-05 11:23:48 +0100

    IJPS-48: Updates for app insight connection string

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -3,15 +3,6 @@ data "azurerm_application_insights" "ai" {
   resource_group_name = "${var.product}-shared-${var.env}"
 }
 
-locals {
-  app_insights_config = jsonencode(
-    merge(
-      jsondecode(
-      file("${path.module}/../lib/applicationinsights.json")), { connectionString = data.azurerm_application_insights.ai.connection_string.value }
-    )
-  )
-}
-
 data "azurerm_key_vault" "key_vault" {
   name                = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-shared-${var.env}"
@@ -19,6 +10,6 @@ data "azurerm_key_vault" "key_vault" {
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = local.app_insights_config
+  value        = data.azurerm_application_insights.ai.connection_string
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
