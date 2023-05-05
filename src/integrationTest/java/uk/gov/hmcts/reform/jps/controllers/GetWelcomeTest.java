c14c0dd5825860aedafb9a319948a6fece0dc727
rebeccabaker <rebecca.baker@hmcts.net>
2023-05-05 15:02:05 +0100

    IJPS-6 resolving some merge conflicts

--- src/integrationTest/java/uk/gov/hmcts/reform/jps/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/jps/controllers/GetWelcomeTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.jps.controllers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.jps.BaseTest;
--- src/integrationTest/java/uk/gov/hmcts/reform/jps/repository/SittingRecordRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/jps/repository/SittingRecordRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.jps.domain.SittingRecord;
 
 import java.time.LocalDate;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
+@ActiveProfiles("itest")
 class SittingRecordRepositoryTest {
 
     @Autowired
