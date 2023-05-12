53fc5bd5c6d6ba373b2f81c306e67c2d56d907e8
marianadpereira <mariana.pereira@hmcts.net>
2023-05-12 10:54:35 +0100

    adding classes for tokens generation

--- src/functionalTest/java/uk/gov/hmcts/reform/jps/StepDefinitions.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/jps/StepDefinitions.java
@@ -3,11 +3,59 @@ package uk.gov.hmcts.reform.jps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import uk.gov.hmcts.reform.jps.testUtil.*;
 
 public class StepDefinitions {
 
+    @Value("${test-url}")
+    protected String testUrl;
+
+    @Autowired
+    protected IdamTokenGenerator idamTokenGenerator;
+
+    @Autowired
+    protected ServiceAuthenticationGenerator serviceAuthenticationGenerator;
+
+    RequestSpecification request;
+    RequestSpecification given;
+    Response response;
+
+//    @Given("a user with the IDAM role of {string}")
+//    public void a_user_with_the_idam_role_of(String string) {
+//        String solicitorToken = idamTokenGenerator.generateIdamTokenForRecorder();
+//
+//        System.out.println(solicitorToken);
+//
+////        request = new RequestSpecBuilder().setBaseUri("http://localhost:3000")
+////            .addHeader("Authorization",solicitorToken)
+////            .setContentType(ContentType.JSON)
+////            .build();
+////        given = given().log().all().spec(request);
+//    }
+
+    @When("a request is prepared with appropriate values")
+    public void a_request_is_prepared_with_appropriate_values() {
+    }
+
     @Given("repo is created")
     public void repo_is_created() {
+//        String s2sToken = serviceAuthenticationGenerator.generate();
+//
+//        System.out.println(s2sToken);
+
+        String recorderToken = idamTokenGenerator.generateIdamTokenForRecorder();
+
+        System.out.println(recorderToken);
+
+//        request = new RequestSpecBuilder().setBaseUri("http://localhost:3000")
+//            .addHeader("Authorization",idamTokenGenerator.generateIdamTokenForRecorder())
+//            .setContentType(ContentType.JSON)
+//            .build();
+//        given = given().log().all().spec(request);
     }
 
     @When("framework is implemented")
---
+++ src/functionalTest/java/uk/gov/hmcts/reform/jps/testUtil/ServiceAuthenticationGenerator.java
@@ -0,0 +1,47 @@
+package uk.gov.hmcts.reform.jps.testUtil;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@TestPropertySource("classpath:application.yaml")
+@Service
+@NoArgsConstructor
+
+public final class ServiceAuthenticationGenerator {
+
+    @Value("${s2s.name}")
+    private String s2sName;
+
+    @Value("${idam.s2s-auth.url}")
+    private String s2sUrl;
+
+    public String generate() {
+        return generate(this.s2sName);
+    }
+
+    public String generate(final String s2sName) {
+        final Response response = RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(s2sUrl)
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .body(Map.of("microservice", s2sName))
+            .when()
+            .post("/testing-support/lease")
+            .andReturn();
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
+
+        return response.getBody().asString();
+    }
+}
