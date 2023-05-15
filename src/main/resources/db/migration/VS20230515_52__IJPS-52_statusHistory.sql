701f35f72bfa565edb5fd3651ceb44cc28044d1c
tomforni <Thomas.Forni@hmcts.net>
2023-05-15 16:49:39 +0100

    Created status history database

---
+++ src/main/resources/db/migration/VS20230515_52__IJPS-52_statusHistory.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE public.status_history (
+	status_history_id bigint not null,
+  sitting_record_ID bigint not null,
+  status_ID date not null,
+  change_date_time timestamp not null,
+  change_by_user_ID status_id varchar(255) not null,
+  CONSTRAINT pk_status_history PRIMARY KEY (status_history_id),
+	CONSTRAINT fk_status_history FOREIGN KEY (sitting_record_ID)
+);
