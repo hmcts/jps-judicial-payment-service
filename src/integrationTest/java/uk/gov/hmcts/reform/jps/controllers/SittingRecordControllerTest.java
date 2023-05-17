f516b35b1fd67dab838cbe468587c3b2fdffb78c
SomeshRA <somesh.acharya1@hmcts.net>
2023-05-17 11:59:50 +0100

    IJPS-42: Removed disable database on startup and commented out search integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/jps/controllers/SittingRecordControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/jps/controllers/SittingRecordControllerTest.java
@@ -2,9 +2,6 @@ package uk.gov.hmcts.reform.jps.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -22,15 +19,10 @@ import static org.testcontainers.shaded.com.google.common.base.Charsets.UTF_8;
 import static org.testcontainers.shaded.com.google.common.io.Resources.getResource;
 
 
-//@WebMvcTest(SittingRecordController.class) // Slicing controller test
-@SpringBootTest
-@AutoConfigureMockMvc
 class SittingRecordControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
     ObjectMapper objectMapper;
 
     //TODO: Need to complete
--- src/main/java/uk/gov/hmcts/reform/jps/Application.java
+++ src/main/java/uk/gov/hmcts/reform/jps/Application.java
@@ -3,10 +3,9 @@ package uk.gov.hmcts.reform.jps;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @OpenAPIDefinition
 @EnableFeignClients(basePackages = {
     "uk.gov.hmcts.reform.jps.refdata.location.client",
