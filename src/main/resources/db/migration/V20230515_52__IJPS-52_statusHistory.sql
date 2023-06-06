d8b31be2559f3524e9636a1c8e79f7a50a01629f
rebeccabaker <rebecca.baker@hmcts.net>
2023-06-06 12:56:53 +0100

    IJPS-52 update sql

--- src/main/resources/db/migration/V20230515_52__IJPS-52_statusHistory.sql
+++ src/main/resources/db/migration/V20230515_52__IJPS-52_statusHistory.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.status_history (
     status_history_id bigserial constraint pk_status_history primary key,
-    sitting_record_id bigserial not null,
+    sitting_record_id bigint not null,
     status_id varchar(255) not null,
     change_date_time timestamp not null,
     change_by_user_id varchar(255) not null,
