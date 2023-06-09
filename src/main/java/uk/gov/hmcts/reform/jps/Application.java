f772381b01825a99a6825018ea3fb50cbe27a3b2
SomeshRA <somesh.acharya1@hmcts.net>
2023-06-09 16:45:01 +0100

    IJPS-37: Removed EnableJpaRepositories import

--- src/main/java/uk/gov/hmcts/reform/jps/Application.java
+++ src/main/java/uk/gov/hmcts/reform/jps/Application.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
