2226f3ef5c097142354c0527ba029fa6c911c43d
rebeccabaker <rebecca.baker@hmcts.net>
2023-06-13 14:47:17 +0100

    IJPS-47 tidy up

--- src/main/java/uk/gov/hmcts/reform/jps/controllers/TestController.java
+++ src/main/java/uk/gov/hmcts/reform/jps/controllers/TestController.java
@@ -2,12 +2,15 @@ package uk.gov.hmcts.reform.jps.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.jps.data.SecurityUtils;
+import uk.gov.hmcts.reform.jps.exceptions.UnauthorisedException;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -23,9 +26,12 @@ public class TestController {
     }
 
     @GetMapping(path = "/test", produces = APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasAnyAuthority('jps-recorder', 'jps-submitter', 'jps-publisher', 'jps-admin')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity getUserRoles() {
+        List<String> allowedRoles = Arrays.asList("jps-recorder", "jps-submitter", "jps-publisher", "jps-admin");
+        securityUtils.getUserInfo().getRoles().stream()
+            .filter(role -> allowedRoles.contains(role)).findFirst()
+            .orElseThrow(() -> new UnauthorisedException("you do not have the correct roles to access this endpoint"));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
