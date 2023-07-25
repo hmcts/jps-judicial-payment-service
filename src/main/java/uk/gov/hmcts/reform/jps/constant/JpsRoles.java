4da5b8cb2da4ba0ba7dd23d512df9cfa0fafe2c2
SomeshRA <somesh.acharya1@hmcts.net>
2023-07-25 15:19:28 +0100

    IJPS-44: Added final constant class instead of interface

--- src/main/java/uk/gov/hmcts/reform/jps/constant/JpsRoles.java
+++ src/main/java/uk/gov/hmcts/reform/jps/constant/JpsRoles.java
@@ -1,9 +1,14 @@
 package uk.gov.hmcts.reform.jps.constant;
 
-public interface JpsRoles {
-    String JPS_RECORDER = "jps-recorder";
-    String JPS_SUBMITTER = "jps-submitter";
-    String JPS_PUBLISHER = "jps-publisher";
-    String JPS_JOH_ADMIN = "jps-JOH-admin";
-    String JPS_ADMIN = "jps-admin";
+
+public final class JpsRoles {
+    private JpsRoles() {
+
+    }
+
+    public static final String JPS_RECORDER = "jps-recorder";
+    public static final String JPS_SUBMITTER = "jps-submitter";
+    public static final  String JPS_PUBLISHER = "jps-publisher";
+    public static final  String JPS_JOH_ADMIN = "jps-JOH-admin";
+    public static final  String JPS_ADMIN = "jps-admin";
 }
