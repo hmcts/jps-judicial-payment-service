9429deaff066354036dc9b3b8d79d61acafb9bf3
SomeshRA <somesh.acharya1@hmcts.net>
2023-07-28 14:41:10 +0100

    IJPS-77: Fixed checkstyle

--- src/main/java/uk/gov/hmcts/reform/jps/services/SittingRecordService.java
+++ src/main/java/uk/gov/hmcts/reform/jps/services/SittingRecordService.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.jps.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
