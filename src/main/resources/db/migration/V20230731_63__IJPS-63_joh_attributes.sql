2ad5c1bae81a39b4a597d8bc5c4d41e5e3b8b953
SomeshRA <69344273+SomeshRA@users.noreply.github.com>
2023-08-24 09:06:24 +0100

    IJPS-63: All columns made mandatory

--- src/main/resources/db/migration/V20230731_63__IJPS-63_joh_attributes.sql
+++ src/main/resources/db/migration/V20230731_63__IJPS-63_joh_attributes.sql
@@ -2,8 +2,8 @@ create table public.joh_attributes (
    joh_attributes_id bigserial constraint pk_joh_attributes primary key,
    local_joh_record_id bigint not null,
    effective_start_date date not null,
-   crown_servant_flag boolean,
-   london_flag boolean
+   crown_servant_flag boolean not null,
+   london_flag boolean not null
 );
 
 ALTER TABLE public.joh_attributes
