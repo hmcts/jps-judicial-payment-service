c6b32f681787147372db25eaa822a7582a45e083
marianadpereira <mariana.pereira@hmcts.net>
2023-08-25 15:08:54 +0100

    fixing files

--- src/functionalTest/java/uk/gov/hmcts/reform/jps/StepDefinitions.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/jps/StepDefinitions.java
@@ -25,7 +25,12 @@ import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 public class StepDefinitions extends TestVariables {
 
