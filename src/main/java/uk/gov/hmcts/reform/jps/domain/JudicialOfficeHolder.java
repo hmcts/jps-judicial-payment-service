64c4a5a691d37de8c2caac4aa4f9e1f98f0583d0
SomeshRA <somesh.acharya1@hmcts.net>
2023-08-31 10:40:53 +0100

    IJPS-77: Merged with master and fixed issues

--- src/main/java/uk/gov/hmcts/reform/jps/domain/JudicialOfficeHolder.java
+++ src/main/java/uk/gov/hmcts/reform/jps/domain/JudicialOfficeHolder.java
@@ -2,17 +2,14 @@ package uk.gov.hmcts.reform.jps.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.hibernate.annotations.Formula;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -26,9 +23,7 @@ import javax.persistence.Table;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
+@Data
 @Entity
 @Table(name = "judicial_office_holder")
 public class JudicialOfficeHolder {
@@ -42,7 +37,6 @@ public class JudicialOfficeHolder {
     @Column(name = "personal_code")
     private String personalCode;
 
-    @ToString.Exclude
     @OneToMany(mappedBy = "judicialOfficeHolder",
         cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private final List<JohPayroll> johPayrolls = new ArrayList<>();
@@ -69,23 +63,5 @@ public class JudicialOfficeHolder {
     public void addJohAttributes(JohAttributes johAttributes) {
         this.johAttributes.add(johAttributes);
         johAttributes.setJudicialOfficeHolder(this);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o instanceof JudicialOfficeHolder that) {
-            return Objects.equals(id, that.id) && Objects.equals(personalCode, that.personalCode) && Objects.equals(
-                johPayrolls, that.johPayrolls) && Objects.equals(johAttributes, that.johAttributes);
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, personalCode, johPayrolls, johAttributes);
     }
 }
