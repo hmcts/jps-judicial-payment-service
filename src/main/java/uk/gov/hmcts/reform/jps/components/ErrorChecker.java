beba63e1ac104751d8faceec08a68517971f81cf
markdathorne <mark.dathorne@cgi.com>
2024-01-03 16:10:41 +0000

    IJPS-115 remove logger debug

--- src/main/java/uk/gov/hmcts/reform/jps/components/ErrorChecker.java
+++ src/main/java/uk/gov/hmcts/reform/jps/components/ErrorChecker.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.jps.components;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.reform.jps.domain.SittingRecordPublishProjection.SittingRecordPublishFields;
 import uk.gov.hmcts.reform.jps.model.PublishErrors;
 
@@ -10,7 +8,6 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 public abstract class ErrorChecker implements PublishErrorChecker {
-    protected static Logger LOGGER = LoggerFactory.getLogger(ErrorChecker.class);
     protected PublishErrorChecker publishErrorChecker;
 
     @Override
@@ -26,8 +23,6 @@ public abstract class ErrorChecker implements PublishErrorChecker {
                          String hmctsServiceCode,
                          SittingRecordPublishFields sittingRecord,
                          Supplier<Optional<?>> evaluated) {
-        LOGGER.debug("evaluate");
-
         Optional<?> isValid = evaluated.get();
         if (isValid.isEmpty()) {
             publishErrors.setError(true);
