4a1027675b93a023d33fca204b041a82c5ed184d
markdathorne <mark.dathorne@cgi.com>
2024-01-03 16:07:14 +0000

    IJPS-115 sql tidy up

--- src/integrationTest/resources/sql/insert_joh.sql
+++ src/integrationTest/resources/sql/insert_joh.sql
@@ -3,17 +3,19 @@ VALUES ('4923421');
 INSERT INTO public.judicial_office_holder ( personal_code)
 VALUES ( '5123421');
 
-INSERT INTO public.joh_attributes ( local_joh_record_id, effective_start_date, crown_servant_flag,
-                                   london_flag)
+INSERT INTO public.joh_attributes
+(local_joh_record_id, effective_start_date, crown_servant_flag, london_flag)
 VALUES ( 1, '2023-09-14', true, false);
-INSERT INTO public.joh_attributes (joh_attributes_id, local_joh_record_id, effective_start_date, crown_servant_flag,
-                                   london_flag)
-VALUES (2, 2, '2023-09-14', false, true);
 
-INSERT INTO public.joh_payroll (joh_payroll_id, local_joh_record_id, effective_start_date, judge_role_type_id,
-                                payroll_id)
-VALUES (1, 1, '2023-09-14', 'Judge', '123');
-INSERT INTO public.joh_payroll (joh_payroll_id, local_joh_record_id, effective_start_date, judge_role_type_id,
-                                payroll_id)
-VALUES (2, 2, '2023-09-14', 'Health', '345');
+INSERT INTO public.joh_attributes
+(local_joh_record_id, effective_start_date, crown_servant_flag, london_flag)
+VALUES (2, '2023-09-14', false, true);
+
+INSERT INTO public.joh_payroll
+(local_joh_record_id, effective_start_date, judge_role_type_id, payroll_id)
+VALUES (1, '2023-09-14', 'Judge', '123');
+
+INSERT INTO public.joh_payroll
+(local_joh_record_id, effective_start_date, judge_role_type_id, payroll_id)
+VALUES (2, '2023-09-15', 'Health', '345');
 
